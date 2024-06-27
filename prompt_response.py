3c76ffbda0cdc401ee2464a4d10dc9b8b3f2a48c
Siddartha <siddarthaj@vueops.com>
2024-06-27 10:14:41 +0530

    Add the files

--- prompt_response.py
+++ prompt_response.py
@@ -4,6 +4,37 @@ from flask_allowedhosts import limit_hosts
 from query import rag_chain
 from es_client import create_es_client
 from constants import *
+import os
+from dotenv import load_dotenv
+from boto3 import client
+from langchain_openai import OpenAIEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_chroma import Chroma
+
+load_dotenv()
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model="text-embedding-3-large")
+text_splitter = SemanticChunker(
+    embeddings, 
+    breakpoint_threshold_type="gradient",
+)
+
+def get_client():
+    return client(
+        's3',
+        'us-east-1',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    )
+def read_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        pdf_reader = PdfReader(file)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        file.close()
+    return text
 
 app = Flask(__name__)
 CORS(app)
@@ -17,10 +48,51 @@ def ask():
     data = request.get_json()
     question = data.get('prompt')
     def generate():
-        for result in rag_chain.stream(question):
-            yield result
-        yield '\n'
+        useful_links = []
+        for result in rag_chain.stream({ "input": question },  {'configurable': {'session_id': 1}}):
+            if 'context' in result:
+                for doc in result['context']:
+                    metadata = doc.metadata
+                    if 'filename' in metadata:
+                        useful_links.append(metadata['filename'])
+            if 'answer' in result:
+                yield result['answer']
+
+        if len(useful_links) > 0:
+            yield "\n\n"
+            yield '<strong> Useful links are </strong> : \n'
+            for link in useful_links:
+                yield '<a href="#">' + link + '</a>'
+                yield '\n'
     return Response(stream_with_context(generate()), mimetype='application/json')
+
+@app.route('/generate_embeddings', methods=['GET'])
+@limit_hosts(allowed_hosts=ALLOWED_HOSTS, on_denied=on_denied)
+def generate_embeddings():
+    document_uid = request.args.get('document_uid')
+
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        temp.write(document_uid.encode())
+        temp_name = temp.name
+        s3_client = get_client()
+        try:
+            s3_client.download_file(os.getenv('DMS_S3_BUCKET'), document_uid, temp_name)
+            pdf_text = read_pdf(temp_name)
+            docs = text_splitter.create_documents([pdf_text])
+            documents = []
+            for doc in docs:
+                doc.metadata = metadata
+                documents.append(doc)
+            Chroma.from_documents(
+                documents,
+                embeddings,
+                persist_directory="./vectorstore",
+            )
+        except Exception as e:
+            return str(e), 500
+        finally:
+            os.unlink(temp_name)
+    return "Embeddings generated successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- query.py
+++ query.py
@@ -5,6 +5,14 @@ import os
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.chains import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 
 load_dotenv()
 
@@ -13,19 +21,57 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model="text-embedding-3-large")
 
 vectordb = Chroma(persist_directory="./vectorstore", embedding_function=embeddings)
-retriever = vectordb.as_retriever(search_type='similarity', search_kargs={'k': 1000})
+retriever = vectordb.as_retriever(search_type='similarity', search_kargs={'k': 1})
+create_history_aware_retriever
+
 
 llm = ChatOpenAI(model="gpt-4-turbo", openai_api_key=openai_api_key)
 
-prompt_template = PromptTemplate.from_template("""                              
-Mechanical, Electrical, and Plumbing (MEP) Engineering Question Answering System, maintenance related questions.                                                        
-Context: {context}
-Question: {question}
-""")
+### Contextualize question ###
+contextualize_q_system_prompt = (
+    "Mechanical, Electrical, and Plumbing (MEP) systems related questions are only answered."
+)
 
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt_template
-    | llm
-    | StrOutputParser()
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt,
+)
+
+### Answer question ###
+system_prompt = (
+    "Mechanical, Electrical, and Plumbing (MEP) systems related questions are only answered."
+    "\n\n"
+    "{context}"
+)
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+init_rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+rag_chain = RunnableWithMessageHistory(
+    init_rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
 )
